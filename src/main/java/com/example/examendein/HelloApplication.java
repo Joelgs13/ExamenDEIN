305e22c082217843d75b02a699d46ff0fec70bf8
Joelgs13 <050213jgonzalez@gmail.com>
2024-11-08 09:17:49 +0100

    configuracion inicial de la app

--- src/main/java/com/example/examendein/HelloApplication.java
+++ src/main/java/com/example/examendein/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.examendein;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/examendein/fxml/productos.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/examendein/img/carrito.png"))); // Establecer el ícono de la ventana
+        stage.setTitle("Productos");
         stage.setScene(scene);
         stage.show();
     }
